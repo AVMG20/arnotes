bf726ce0002aeba63b1b4ac1e6f8a97cf6161b9d
Arno Visker <arno@bigbridge.nl>
2026-06-05 15:44:22 +0200

    update pwa settings

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,10 @@
 <script setup>
 useHead({
   htmlAttrs: { lang: 'en' },
-  link: [{ rel: 'icon', href: '/favicon.ico' }]
+  link: [
+    { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+    { rel: 'apple-touch-icon', href: '/apple-touch-icon-180x180.png' }
+  ]
 })
 useSeoMeta({ title: 'Notes' })
 </script>
